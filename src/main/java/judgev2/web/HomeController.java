c0afe57c77122165c58c61b84010484ba9f2655e
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-05-09 18:59:48 +0300

    Index & Register Get Controllers, Th Fragments

---
+++ src/main/java/judgev2/web/HomeController.java
@@ -0,0 +1,14 @@
+package judgev2.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    private String index() {
+        return "index";
+    }
+
+}
